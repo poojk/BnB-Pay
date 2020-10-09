ee5d2e136164880e2f7fbb0779e3687afa24da9d
Raveena Pooja Kankeyan <66836884+poojk@users.noreply.github.com>
2020-10-09 07:28:24 -0400

    Create bnbclean.py

---
+++ bnbclean.py
@@ -0,0 +1,40 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
+from functools import reduce
+from pyspark.sql import DataFrame
+
+spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL basic example") \
+        .config("spark.jars", "/usr/local/spark/postgresql-42.2.16.jar") \
+        .getOrCreate()
+
+df = spark.read \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://10.0.0.8:5432/my_db") \
+        .option("dbtable", "airbnb") \
+        .option("user", "test") \
+        .option("password", "test") \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+
+df = df.withColumn('bedrooms',F.round(df['bedrooms'],0))
+df = df.filter(~F.col('city').contains("/"))
+df = df.filter(~F.col('city').contains(","))
+df = df.filter(~F.col('city').contains("-"))
+df = df.filter(~F.col('city').contains("^[0-9]*$"))
+df = df.filter(~F.col('city').contains("*"))
+df = df.withColumn('city', F.ltrim(df.city))
+df = df.filter(~F.col('city').contains("("))
+df = df.groupBy('city','bedrooms').agg(F.avg('average').alias('average'))
+df = df.sort('city')
+df1 = df.withColumn('average', F.round(df['average'], 0))
+#agg_df.show()
+
+df1.write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://10.0.0.8:5432/my_db") \
+        .option("dbtable", "bnbclean") \
+        .option("user", "test") \
